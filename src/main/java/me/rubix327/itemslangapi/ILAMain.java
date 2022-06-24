50a69371580c48574fa43c01163ab458c58fe339
Rubix327 <yarik.cool1@mail.ru>
2022-06-24 21:42:10 +0300

    Fixed defining legacy version.

---
+++ src/main/java/me/rubix327/itemslangapi/ILAMain.java
@@ -0,0 +1,11 @@
+package me.rubix327.itemslangapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ILAMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
